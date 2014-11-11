3c4f1a173956bb90b7237367eeed8368ef3f7990
Michael Bingham <mbingham@hw10536.gsm1900.org>
2014-11-10 17:22:46 -0800

    Added use of Lucene analyzer to Word Count example

--- cascalog/wordcount/src/wordcount.clj
+++ cascalog/wordcount/src/wordcount.clj
@@ -4,15 +4,27 @@
   (:require [clojure.string :as s]
             [cascalog.logic.def :as def]
             [cascalog.logic.ops :as c])
+  (:import (org.apache.lucene.analysis TokenStream)
+           (org.apache.lucene.analysis.en EnglishAnalyzer)
+           (org.apache.lucene.analysis.tokenattributes CharTermAttribute)
+           (java.io StringReader))
   (:gen-class))
 
+
 (def/defmapcatfn tokenize [line]
-  "reads in a line of string and tokenizes it by regex"
-  (s/split line #"[\[\]\\\(\),.)\s]+"))
+                 "reads in a line of string and tokenizes it using a Lucene Analyzer"
+                 (let [tokenStream (.tokenStream (EnglishAnalyzer.) "contents" (StringReader. line))
+                       attributeType CharTermAttribute
+                       term (.addAttribute tokenStream attributeType)]
+                   (do (.reset tokenStream)
+                       (loop [result []]
+                         (if (.incrementToken tokenStream)
+                           (recur (conj result (str term)))
+                           (do (.close tokenStream) result))))))
 
 (defn -main [in out & args]
   (?<- (hfs-delimited out)
-    [?word ?count]
-    ((hfs-delimited in :skip-header? true) ?line)
-    (tokenize ?line :> ?word)
-    (c/count ?count)))
+       [?word ?count]
+       ((hfs-delimited in :skip-header? true) ?line)
+       (tokenize ?line :> ?word)
+       (c/count ?count)))
