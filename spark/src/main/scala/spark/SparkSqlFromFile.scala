90ef037676d7e41829fa4a6b91141ea9d5c35546
Michael Bingham <michael.b.bingham@gmail.com>
2015-08-25 15:43:05 +0800

    Added Spark Streaming demo with Kafka

--- spark/src/main/scala/spark/SparkSqlFromFile.scala
+++ spark/src/main/scala/spark/SparkSqlFromFile.scala
@@ -1,5 +1,6 @@
 package spark
 
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkSqlFromFile {
@@ -10,14 +11,14 @@ object SparkSqlFromFile {
     val optionArgs = args.lift
     val conf = new SparkConf().setAppName("SalaryData Spark SQL from file")
     val sc = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val sqlContext = new HiveContext(sc)
 
-    import sqlContext.createSchemaRDD
+    import sqlContext.implicits._
 
     val salaries = sc.textFile(optionArgs(1).getOrElse("hdfs:///user/root/salarydata.txt")).map(_.split(","))
       .map(s => SalaryData(s(0).trim, s(1).trim.toInt, s(2).trim.toDouble, s(3).trim.toInt))
 
-    salaries.registerTempTable("salaries")
+    salaries.toDF().registerTempTable("salaries")
 
     val genderBreakdown = sqlContext.sql("select gender, count(*) from salaries group by gender")
     genderBreakdown.collect().foreach(println)
--- spark/src/main/scala/spark/SparkSqlFromHive.scala
+++ spark/src/main/scala/spark/SparkSqlFromHive.scala
@@ -1,12 +1,13 @@
 package spark
 
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkSqlFromHive {
    def main(args: Array[String]) {
      val conf = new SparkConf().setAppName("SalaryData Spark SQL from Hive")
      val sc = new SparkContext(conf)
-     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
+     val sqlContext = new HiveContext(sc)
 
      val genderBreakdown = sqlContext.sql("select gender, count(*) from salaries group by gender")
      genderBreakdown.collect().foreach(println)
